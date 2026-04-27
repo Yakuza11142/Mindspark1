2788b5c5ae4e6bedcf4582a9c3cdedfff087bc02
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-27 22:21:01 +0100

    Create lib/services/share_service.dart

---
+++ lib/services/share_service.dart
@@ -0,0 +1,7 @@
+import 'package:share_plus/share_plus.dart';
+
+class ShareService {
+  static void inviteFriend() {
+    Share.share("I'm learning 3D Science on MindSpark! Download here: https://mindspark.app");
+  }
+}
