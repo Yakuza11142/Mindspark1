c1d68471c527b1ba12bf38f18cd95b6bd76a72a4
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-27 22:02:13 +0100

    Create lib/utils/constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const bg = Color(0xFF0F172A);
+  static const accent = Colors.cyanAccent;
+  static const pro = Colors.amber;
+}
