8bc73a2a47f7e1d90255248cd83caafee9e04b71
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-27 22:53:10 +0100

    Update security_service.dart

--- lib/services/security_service.dart
+++ lib/services/security_service.dart
@@ -4,3 +4,28 @@ class SecurityService {
     return true; // Use flutter_jailbreak_detection in prod
   }
 }
+import 'package:flutter/services.dart';
+import 'package:flutter_jailbreak_detection/flutter_jailbreak_detection.dart';
+
+class SecurityService {
+  // 1. CHECK FOR HACKERS (Root/Jailbreak)
+  static Future<bool> isDeviceCompromised() async {
+    bool jailbroken = false;
+    bool developerMode = false;
+
+    try {
+      jailbroken = await FlutterJailbreakDetection.jailbroken;
+      // We allow developer mode for you to test, but you can block it later
+      // developerMode = await FlutterJailbreakDetection.developerMode; 
+    } on PlatformException {
+      jailbroken = true; // Assume the worst if check fails
+    }
+
+    return jailbroken;
+  }
+
+  // 2. KILL SWITCH
+  static void killApp() {
+    SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+  }
+}
