e6db74c9f02e3b10bb5991c584a5340f74438434
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-28 18:38:52 +0100

    Create lib/models/language_model.dart

---
+++ lib/models/language_model.dart
@@ -0,0 +1,7 @@
+class LanguageModel {
+  final String name;
+  final String code; // ISO Code (e.g., 'yo' for Yoruba, 'sux' for Sumerian)
+  final String flag;
+
+  LanguageModel(this.name, this.code, this.flag);
+}
