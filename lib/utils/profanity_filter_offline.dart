ffae4f3bb7069549946740e87baffedba10505ca
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-28 07:05:53 +0100

    Create lib/utils/profanity_filter_offline.dart

---
+++ lib/utils/profanity_filter_offline.dart
@@ -0,0 +1,6 @@
+class ProfanityFilterOffline {
+  static const List<String> list =[
+    "fuck", "shit", "bitch", "cunt", "ass", "dick", "pussy", "porn", "nude", "blowjob",
+    "olosho", "ashawo", "mumu", "ode" // Nigerian context
+  ];
+}
