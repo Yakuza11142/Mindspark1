f822e3f40e38f2bdb6610e398a450f965cbe6cd6
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-28 19:37:46 +0100

    Update mic_permission_gate.dart

--- lib/services/mic_permission_gate.dart
+++ lib/services/mic_permission_gate.dart
@@ -9,3 +9,14 @@ class MicPermissionGate {
     return status.isGranted;
   }
 }
+import 'package:permission_handler/permission_handler.dart';
+
+class MicPermissionGate {
+  static Future<bool> ensureMicAccess() async {
+    PermissionStatus status = await Permission.microphone.status;
+    if (!status.isGranted) {
+      status = await Permission.microphone.request();
+    }
+    return status.isGranted;
+  }
+}
