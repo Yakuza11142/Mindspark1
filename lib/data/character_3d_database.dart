70b1bc979cbde25ad559af17ffed6a0e4e115976
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-28 18:05:12 +0100

    Create lib/data/character_3d_database.dart

---
+++ lib/data/character_3d_database.dart
@@ -0,0 +1,7 @@
+import '../models/volumetric_avatar.dart';
+
+class Character3dDatabase {
+  static final Map<String, VolumetricAvatar> avatars = {
+    "Spark": VolumetricAvatar("Spark", "assets/models/spark_american.glb", "Idle_Hype", "Talk_Hype"),
+  };
+}
