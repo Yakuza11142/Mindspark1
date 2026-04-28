595c971de4a817a0a25b1ef9f3f8cc7f21ea4852
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-28 19:07:43 +0100

    Create lib/engines/mesh_decimation_service.dart

---
+++ lib/engines/mesh_decimation_service.dart
@@ -0,0 +1,5 @@
+class MeshDecimationService {
+  static void reduceQualityForLowEndPhones() {
+    print("📉 Low RAM detected. Lowering polygon count on Trousers and Shoes by 50%. Face quality maintained.");
+  }
+}
