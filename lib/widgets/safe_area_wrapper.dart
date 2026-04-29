b96c8cace4d1a5edc039707757adf56bed8563e7
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-29 22:52:45 +0100

    Update safe_area_wrapper.dart

--- lib/widgets/safe_area_wrapper.dart
+++ lib/widgets/safe_area_wrapper.dart
@@ -5,3 +5,10 @@ class SafeAreaWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) => SafeArea(child: child);
 }
+import 'package:flutter/material.dart';
+class SafeAreaWrapper extends StatelessWidget {
+  final Widget child;
+  const SafeAreaWrapper({super.key, required this.child});
+  @override
+  Widget build(BuildContext context) => SafeArea(child: child);
+}
