4feab525201de5e846d5846c0521eba1cc8c9cb7
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-30 16:41:24 +0100

    Create lib/widgets/dynamic_app_icon_switcher.dart

---
+++ lib/widgets/dynamic_app_icon_switcher.dart
@@ -0,0 +1,6 @@
+// Requires flutter_dynamic_icon package
+class DynamicAppIconSwitcher {
+  static void switchToGoldIcon() {
+    print("👑 Pro User detected. Changing Android/iOS App Icon to 'MindSpark Gold'.");
+  }
+}
