cea3f0107c7f2800f20bc3ab19c21ec14dc9e77d
Yakuza11142 <yakubupeter785@gmail.com>
2026-04-30 18:12:09 +0100

    Create lib/web/pdf_web_viewer.dart

---
+++ lib/web/pdf_web_viewer.dart
@@ -0,0 +1,7 @@
+// Requires printing/pdf package web implementations
+class PdfWebViewer {
+  static void openInNewTab(String pdfDataUrl) {
+    print("Opening PDF Certificate in a new browser tab.");
+    // Implementation uses html.window.open
+  }
+}
