f0c4e10fdc290f0b0092adbaf819f5ea191c64c6
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 20:11:31 +0100

    Create lib/engines/group_study_matcher.dart

---
+++ lib/engines/group_study_matcher.dart
@@ -0,0 +1,6 @@
+class GroupStudyMatcher {
+  static String findAfterSchoolLobby(String subject) {
+    return "Searching for other students studying $subject right now...";
+    // Connects to Supabase real-time presence
+  }
+}
