19cc061aba3486b67886c5a10b69ccdbe514dca6
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 19:12:07 +0100

    Create lib/engines/mnemonics_generator_prompt.dart

---
+++ lib/engines/mnemonics_generator_prompt.dart
@@ -0,0 +1,5 @@
+class MnemonicsGeneratorPrompt {
+  static String inject(String topic) {
+    return "Generate a catchy, easy-to-remember Mnemonic or Acronym for the key principles/elements of '$topic'. Make the acronym a real word if possible.";
+  }
+}
