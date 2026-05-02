e938189e877620f762066fc2c44208f4f179c384
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 22:41:20 +0100

    Create lib/data/global_curriculum_tags.dart

---
+++ lib/data/global_curriculum_tags.dart
@@ -0,0 +1,8 @@
+class GlobalCurriculumTags {
+  static const Map<String, String> promptTags = {
+    "US": "[ALIGN WITH: US Common Core Standards]",
+    "GB": "[ALIGN WITH: UK National Curriculum]",
+    "IN": "[ALIGN WITH: CBSE/ICSE Board Syllabus]",
+    "NG": "[ALIGN WITH: NERDC Curriculum]"
+  };
+}
