e02e1a9bed8ec683da1d275727599d890852789b
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 13:26:55 +0100

    Create lib/animations/seamless_ui_transitions.dart

---
+++ lib/animations/seamless_ui_transitions.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SeamlessUiTransitions extends PageTransitionsBuilder {
+  @override
+  Widget buildTransitions<T>(PageRoute<T> route, BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    return FadeTransition(opacity: animation, child: child);
+  }
+}
