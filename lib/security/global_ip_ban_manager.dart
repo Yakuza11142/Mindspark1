6e02d7552c3aec5571f0b5ee51b455d6bce3d78a
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 22:39:20 +0100

    Create lib/security/global_ip_ban_manager.dart

---
+++ lib/security/global_ip_ban_manager.dart
@@ -0,0 +1,6 @@
+class GlobalIpBanManager {
+  static bool isIpBanned(String ipAddress) {
+    // Connects to Supabase to check if this IP is blacklisted globally for API abuse.
+    return false;
+  }
+}
