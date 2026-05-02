023e272dbb9ac937c9401c4b437fb8e6900b130f
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 13:35:22 +0100

    Create lib/network/bluetooth_mesh_broadcaster.dart

---
+++ lib/network/bluetooth_mesh_broadcaster.dart
@@ -0,0 +1,6 @@
+class BluetoothMeshBroadcaster {
+  static void advertisePresence(String username) {
+    print("🔵 Broadcasting Bluetooth LE Beacon: MindSpark_User_$username");
+    // Implementation requires flutter_blue_plus
+  }
+}
