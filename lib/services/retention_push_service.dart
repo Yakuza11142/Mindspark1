0fde6385441cf5184a7217eb6fe24e2cf3a939b0
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 22:53:08 +0100

    Create lib/services/retention_push_service.dart

---
+++ lib/services/retention_push_service.dart
@@ -0,0 +1,5 @@
+class RetentionPushService {
+  static void triggerSaveCampaign(String examTarget) {
+    print("🚨 PUSH NOTIFICATION: Your $examTarget competition is studying right now. Open MindSpark to keep your edge.");
+  }
+}
