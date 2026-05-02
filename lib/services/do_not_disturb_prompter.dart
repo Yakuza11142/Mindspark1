7a0dce68a21a8d432a3ba5648f44d6ad7df17a9c
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 20:53:34 +0100

    Create lib/services/do_not_disturb_prompter.dart

---
+++ lib/services/do_not_disturb_prompter.dart
@@ -0,0 +1,5 @@
+class DoNotDisturbPrompter {
+  static void prompt() {
+    print("🔔 PUSH: Your 2-Hour Mock Exam is starting. Please turn on 'Do Not Disturb' to silence WhatsApp messages.");
+  }
+}
