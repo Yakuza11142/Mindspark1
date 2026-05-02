c45c86de90f099e1ec73f1890e8c834d74d16ad9
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-02 14:03:48 +0100

    Create lib/widgets/ai_thinking_dots.dart

---
+++ lib/widgets/ai_thinking_dots.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class AiThinkingDots extends StatelessWidget {
+  const AiThinkingDots({super.key});
+  @override
+  Widget build(BuildContext context) => const Text("...", style: TextStyle(fontSize: 24, color: Colors.cyanAccent, letterSpacing: 2));
+}
