4dbce9eeb1e8101a5d30d78ea347790766b283e3
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 12:33:56 +0100

    Create lib/services/classroom_offline_leaderboard.dart

---
+++ lib/services/classroom_offline_leaderboard.dart
@@ -0,0 +1,6 @@
+class ClassroomOfflineLeaderboard {
+  static void broadcastScoreToNearbyPhones(int score) {
+    // Uses Google Nearby Connections API to beam scores to classmates in the same room.
+    print("Broadcasting score: $score to 14 nearby devices.");
+  }
+}
