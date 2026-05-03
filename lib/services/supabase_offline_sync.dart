48e0cc7942596b42ce2d28a04bd24b9ce37bcd3b
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 21:52:08 +0100

    Create lib/services/supabase_offline_sync.dart

---
+++ lib/services/supabase_offline_sync.dart
@@ -0,0 +1,6 @@
+class SupabaseOfflineSync {
+  static void syncNow() {
+    print("🌍 Network restored. Syncing local scores to Supabase Cloud...");
+    // Read local cache and batch insert to Supabase
+  }
+}
