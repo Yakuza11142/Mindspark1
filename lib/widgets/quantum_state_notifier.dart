d5bdceeae03f18e2a72aa88bea97bca477b13908
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 17:36:58 +0100

    Create lib/widgets/quantum_state_notifier.dart

---
+++ lib/widgets/quantum_state_notifier.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class QuantumStateNotifier extends StatelessWidget {
+  const QuantumStateNotifier({super.key});
+  @override
+  Widget build(BuildContext context) => const Icon(Icons.shield, color: Colors.greenAccent, size: 14);
+}
