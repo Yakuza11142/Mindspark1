911db066864a00a1f6dda8d97e90337ad63766ca
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 22:12:35 +0100

    Create lib/config/supabase_tables.dart

---
+++ lib/config/supabase_tables.dart
@@ -0,0 +1,6 @@
+class SupabaseTables {
+  static const String profiles = 'profiles';
+  static const String examHistory = 'exam_history';
+  static const String verifiedCerts = 'verified_certificates';
+  static const String crashLogs = 'crash_logs';
+}
