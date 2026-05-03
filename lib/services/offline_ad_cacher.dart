1c70bb4089b06115da6ceccdfe5b75887ca44c7e
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 14:42:49 +0100

    Create lib/services/offline_ad_cacher.dart

---
+++ lib/services/offline_ad_cacher.dart
@@ -0,0 +1,8 @@
+class OfflineAdCacher {
+  static void cacheAdsWhileOnWifi() {
+    print("Caching 5 AdMob videos to local storage for offline viewing.");
+  }
+  static void reportOfflineWatchesWhenOnline() {
+    print("Syncing offline ad views to Google to claim revenue.");
+  }
+}
