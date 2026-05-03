9763a9cb9c5e6c056a23f71a61e591b74b8e2362
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 12:36:50 +0100

    Create lib/engines/apk_xender_beacon.dart

---
+++ lib/engines/apk_xender_beacon.dart
@@ -0,0 +1,7 @@
+// Requires 'apk_extractor' or native channel
+class ApkXenderBeacon {
+  static void generateExtractionQr() {
+    print("📡 Extracting com.mindspark.elite.apk from device storage...");
+    print("QR Code ready. Scan to download locally without data.");
+  }
+}
