8cbd2f34fe899043a3bcd392eac1d04f9ca0828d
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 14:02:40 +0100

    Create lib/engines/ar_spatial_anchor.dart

---
+++ lib/engines/ar_spatial_anchor.dart
@@ -0,0 +1,5 @@
+class ArSpatialAnchor {
+  static void pinFactToWall(String fact, double x, double y, double z) {
+    print("📍 Spatial Anchor Set: Pinned '$fact' to physical coordinates ($x, $y, $z) in user's room.");
+  }
+}
