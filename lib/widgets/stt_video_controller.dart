7ea078629da1cf808425bd3fa310d11c947dea00
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 13:09:25 +0100

    Create lib/widgets/stt_video_controller.dart

---
+++ lib/widgets/stt_video_controller.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class SttVideoController extends StatelessWidget {
+  const SttVideoController({super.key});
+  @override
+  Widget build(BuildContext context) => const Icon(Icons.mic_external_on, color: Colors.amber);
+}
