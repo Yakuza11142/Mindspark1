c48327ea59da06879170048587079f38ab2b4e54
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-03 22:59:22 +0100

    Create lib/widgets/daily_retention_bonus.dart

---
+++ lib/widgets/daily_retention_bonus.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class DailyRetentionBonus extends StatelessWidget {
+  const DailyRetentionBonus({super.key});
+  @override
+  Widget build(BuildContext context) => const Text("+50 Sparks Daily Bonus Available!", style: TextStyle(color: Colors.amber));
+}
