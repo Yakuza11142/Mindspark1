ebeea512c28e8ceebcc17228efc008cfcf0ca5e9
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-05 08:41:21 +0100

    Create lib/engines/ar_lighting_enhancer.dart

---
+++ lib/engines/ar_lighting_enhancer.dart
@@ -0,0 +1,6 @@
+class ArLightingEnhancer {
+  static String getStageLightingConfig() {
+    // Injects environmental HDR lighting to the 3D model viewer
+    return "environment-image='neutral' exposure='1.5' shadow-intensity='2'";
+  }
+}
