670802e4a53c3b29ea50d76a587f2ed5705d382d
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-05 09:18:38 +0100

    Create lib/models/study_analytics_model.dart

---
+++ lib/models/study_analytics_model.dart
@@ -0,0 +1,8 @@
+class StudyAnalytics {
+  final int totalMinutes;
+  final int questionsAnswered;
+  final int correctAnswers;
+  final String weakestSubject;
+
+  StudyAnalytics(this.totalMinutes, this.questionsAnswered, this.correctAnswers, this.weakestSubject);
+}
