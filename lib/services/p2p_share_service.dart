e40e444c8d54f3a4f044423ba5f3fe313701ea2b
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-07 15:30:04 +0100

    Create lib/services/p2p_share_service.dart

---
+++ lib/services/p2p_share_service.dart
@@ -0,0 +1,12 @@
+// Requires 'nearby_connections' package
+class P2PShareService {
+  static void startAdvertising() {
+    print("Broadcasting MindSpark signal to nearby devices...");
+    // Logic to open Bluetooth/WiFi Direct advertising
+  }
+
+  static void startDiscovering() {
+    print("Searching for nearby students...");
+    // Logic to scan for local peers
+  }
+}
