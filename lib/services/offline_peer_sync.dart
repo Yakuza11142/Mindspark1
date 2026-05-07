440bfab195cd7e6ef19a1d84c3e5c912e89f6aa4
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-07 15:28:12 +0100

    Create lib/services/offline_peer_sync.dart

---
+++ lib/services/offline_peer_sync.dart
@@ -0,0 +1,6 @@
+class OfflinePeerSync {
+  static void exchangeTokens(String peerData) {
+    // Encrypt and store peer XP locally until internet returns
+    print("Synced offline data with peer.");
+  }
+}
