aa52c10cefbd5faf464c88ea480b837987efca24
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-07 15:38:13 +0100

    Create lib/services/shorts_cache.dart

---
+++ lib/services/shorts_cache.dart
@@ -0,0 +1,6 @@
+class ShortsCache {
+  static void preloadNextVideos(int currentIndex) {
+    // Logic to trigger Pexels API for index + 1 and index + 2
+    print("Preloading next shorts...");
+  }
+}
