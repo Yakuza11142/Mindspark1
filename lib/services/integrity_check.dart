37050da5de6fbdc81873ab2599bd94f2a1d449fc
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-08 21:33:58 +0100

    Create lib/services/integrity_check.dart

---
+++ lib/services/integrity_check.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+// Placeholder for package_info_plus checksum logic
+class IntegrityCheck {
+  static bool verifySignature() {
+    // In production, compare app signature with hardcoded hash
+    return true; 
+  }
+}
