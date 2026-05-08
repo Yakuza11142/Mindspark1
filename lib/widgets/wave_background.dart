822629190fbcd2b336dc5fb57ae1673b1ac75aec
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-08 20:42:54 +0100

    Create lib/widgets/wave_background.dart

---
+++ lib/widgets/wave_background.dart
@@ -0,0 +1,6 @@
+// Placeholder for CustomPainter wave logic
+import 'package:flutter/material.dart';
+class WaveBackground extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(color: const Color(0xFF0F172A));
+}
