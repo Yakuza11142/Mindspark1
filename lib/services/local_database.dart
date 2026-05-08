599ad434363751d5ae44d36dc0d21bb87f2166e9
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-08 23:20:10 +0100

    Create lib/services/local_database.dart

---
+++ lib/services/local_database.dart
@@ -0,0 +1,6 @@
+// Placeholder for sqflite logic
+class LocalDatabase {
+  static Future<void> saveUserProgress(int xp, int level) async {
+    // SQL: INSERT INTO user (xp, level) VALUES (...)
+  }
+}
