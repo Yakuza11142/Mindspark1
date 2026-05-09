680b48cbc2b36e79c868fc25fdb5099534c65977
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-09 12:55:33 +0100

    Create lib/services/deep_link_navigator.dart

---
+++ lib/services/deep_link_navigator.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DeepLinkNavigator {
+  static void navigate(BuildContext context, String path) {
+    if (path.contains("quiz")) {
+      // Nav to quiz
+    }
+  }
+}
