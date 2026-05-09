88df12ecbf4e8a6cd8863e2f8838003a049c169b
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-09 13:12:17 +0100

    Create lib/services/app_lifecycle.dart

---
+++ lib/services/app_lifecycle.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LifecycleEventHandler extends WidgetsBindingObserver {
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      // Save data
+    }
+  }
+}
