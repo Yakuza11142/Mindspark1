b5f68ecfcbb1a344ab5f42d26ba3b459c0496914
Yakuza11142 <yakubupeter785@gmail.com>
2026-05-09 11:57:18 +0100

    Create lib/utils/url_launcher_wrapper.dart

---
+++ lib/utils/url_launcher_wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class UrlLauncherWrapper {
+  static Future<void> open(String url) async {
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    }
+  }
+}
