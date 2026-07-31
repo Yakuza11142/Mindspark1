ad528abebb565621a736d9dd4434f10c8cbe0c49
Yakuza11142 <yakubupeter785@gmail.com>
2026-07-31 23:21:55 +0100

    Delete global_curriculum_tags.dart

--- lib/data/global_curriculum_tags.dart
+++
@@ -1,8 +0,0 @@
-class GlobalCurriculumTags {
-  static const Map<String, String> promptTags = {
-    "US": "[ALIGN WITH: US Common Core Standards]",
-    "GB": "[ALIGN WITH: UK National Curriculum]",
-    "IN": "[ALIGN WITH: CBSE/ICSE Board Syllabus]",
-    "NG": "[ALIGN WITH: NERDC Curriculum]"
-  };
-}
