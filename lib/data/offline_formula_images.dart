316a3e0759ff2d58b07d143d7bc5f9c3a78bdd82
Yakuza11142 <yakubupeter785@gmail.com>
2026-07-31 23:23:52 +0100

    Delete offline_formula_images.dart

--- lib/data/offline_formula_images.dart
+++
@@ -1,6 +0,0 @@
-class OfflineFormulaImages {
-  // Example Base64 string for Benzene Ring
-  static const String benzeneRingB64 =
-      "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==";
-  // Thousands of these can be hardcoded for instant rendering
-}
