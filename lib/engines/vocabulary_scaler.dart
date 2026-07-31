a8af2be06c23ab7d9b947cead43119180d48da04
Yakuza11142 <yakubupeter785@gmail.com>
2026-07-31 23:29:55 +0100

    Delete vocabulary_scaler.dart

--- lib/engines/vocabulary_scaler.dart
+++
@@ -1,8 +0,0 @@
-class VocabularyScaler {
-  static String injectLexileLevel(String prompt, String stage) {
-    if (stage == "JUNIOR") {
-      return "$prompt [Keep Lexile reading measure below 600L. No big words.]";
-    }
-    return prompt;
-  }
-}
