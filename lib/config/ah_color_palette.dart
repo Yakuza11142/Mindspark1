90baa943018ed32fa4f2f5fd4020f6064e32948a
Yakuza11142 <yakubupeter785@gmail.com>
2026-07-31 23:13:52 +0100

    Delete ah_color_palette.dart

--- lib/config/ah_color_palette.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AhColorPalette {
-  static const Color sparkCyan = Color(0xFF00FFFF);
-  static Color getColorForAh(String name) {
-    return sparkCyan; // Default to Spark
-  }
-}
