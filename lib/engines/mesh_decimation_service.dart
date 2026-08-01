a90e7008b88671299817a46a9821bfab720fe174
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 13:56:12 +0100

    Delete mesh_decimation_service.dart

--- lib/engines/mesh_decimation_service.dart
+++
@@ -1,6 +0,0 @@
-class MeshDecimationService {
-  static void reduceQualityForLowEndPhones() {
-    print(
-        "📉 Low RAM detected. Lowering polygon count on Trousers and Shoes by 50%. Face quality maintained.");
-  }
-}
