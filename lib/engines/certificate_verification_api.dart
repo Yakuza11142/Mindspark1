20f6ef3013452d96dd93e523ad2b9ba1dfafdb92
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 17:00:29 +0100

    Delete certificate_verification_api.dart

--- lib/engines/certificate_verification_api.dart
+++
@@ -1,7 +0,0 @@
-class CertificateVerificationApi {
-  static bool verifyCertificate(String certificateId, String studentName) {
-    // In production, this checks your Firebase database
-    print("Verifying Certificate ID: $certificateId for $studentName...");
-    return true; // Valid
-  }
-}
