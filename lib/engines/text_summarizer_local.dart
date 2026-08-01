0ca89472f6978e274991e42a75133b8830108471
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 11:04:51 +0100

    Delete text_summarizer_local.dart

--- lib/engines/text_summarizer_local.dart
+++
@@ -1,9 +0,0 @@
-class TextSummarizerLocal {
-  static String getSummary(String fullText) {
-    List<String> sentences = fullText.split('.');
-    if (sentences.length > 2) {
-      return "${sentences[0]}. ${sentences[1]}.";
-    }
-    return fullText;
-  }
-}
