a6ab75fe774276af9be4f8d97adcf022857e7eae
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 12:14:45 +0100

    Delete rapid_review_prompt_injector.dart

--- lib/engines/rapid_review_prompt_injector.dart
+++
@@ -1,5 +0,0 @@
-class RapidReviewPromptInjector {
-  static String inject(String topic) {
-    return "Explain '$topic'.[CRITICAL SYSTEM OVERRIDE: The user's exam is in 5 days. Output ONLY 3 bullet points containing the most frequently tested facts or formulas on this topic. Maximum 50 words. Do not use pleasantries.]";
-  }
-}
