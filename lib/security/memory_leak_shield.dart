8e86bcb8e30a6ba5a93ec69d7d9644b6257a852f
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 00:47:45 +0100

    Delete memory_leak_shield.dart

--- lib/security/memory_leak_shield.dart
+++
@@ -1,9 +0,0 @@
-class MemoryLeakShield {
-  static void runSafeZone(Function appRunner) {
-    try {
-      appRunner();
-    } catch (e) {
-      print("CRITICAL: Leak prevented. System restored.");
-    }
-  }
-}
