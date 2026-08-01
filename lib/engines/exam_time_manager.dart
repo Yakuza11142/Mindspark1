1caeef787d1a7ec069bf0eb93aa76ea625a1d907
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 16:34:09 +0100

    Delete exam_time_manager.dart

--- lib/engines/exam_time_manager.dart
+++
@@ -1,6 +0,0 @@
-class ExamTimeManager {
-  static int calculatePace(int timeElapsed, int questionsAnswered) {
-    if (questionsAnswered == 0) return 0;
-    return timeElapsed ~/ questionsAnswered; // Seconds per question
-  }
-}
