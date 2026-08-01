77e1fa8f40e4f4b184f74c0d22f6819b26ab0138
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 16:34:37 +0100

    Delete exam_center_survival_bot.dart

--- lib/engines/exam_center_survival_bot.dart
+++
@@ -1,8 +0,0 @@
-class ExamCenterSurvivalBot {
-  static String ask(String query) {
-    if (query.toLowerCase().contains("thumbprint")) {
-      return "If your thumbprint fails, wipe it on your hair to add natural oil, or use hand sanitizer. Do not panic. The officials will assist you.";
-    }
-    return "Focus on the exam. Breathe.";
-  }
-}
