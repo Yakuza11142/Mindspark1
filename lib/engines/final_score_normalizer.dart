6de827270d28c38062b52fbc33ca9ac0ea9010a7
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 16:33:09 +0100

    Delete final_score_normalizer.dart

--- lib/engines/final_score_normalizer.dart
+++
@@ -1,7 +0,0 @@
-class FinalScoreNormalizer {
-  static int applyCurve(int rawScore, int totalQuestions) {
-    double percentage = rawScore / totalQuestions;
-    // JAMB standardizes scores. A raw 60% might curve to a 250/400.
-    return (percentage * 400).round();
-  }
-}
