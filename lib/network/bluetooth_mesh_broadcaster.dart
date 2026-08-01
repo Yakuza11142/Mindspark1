3da2810bd1d658ef487d1536e2981d700fd8e26b
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 20:42:43 +0100

    Delete bluetooth_mesh_broadcaster.dart

--- lib/network/bluetooth_mesh_broadcaster.dart
+++
@@ -1,6 +0,0 @@
-class BluetoothMeshBroadcaster {
-  static void advertisePresence(String username) {
-    print("🔵 Broadcasting Bluetooth LE Beacon: MindSpark_User_$username");
-    // Implementation requires flutter_blue_plus
-  }
-}
