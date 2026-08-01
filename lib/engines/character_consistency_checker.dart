2801a1fee18abe1b02c648647e98444bd5843306
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 17:00:18 +0100

    Delete character_consistency_checker.dart

--- lib/engines/character_consistency_checker.dart
+++
@@ -1,9 +0,0 @@
-class CharacterConsistencyChecker {
-  static String enforce(String aiText, String expectedName) {
-    // If Spark accidentally calls himself Nexus, fix it.
-    if (expectedName == "Spark" && aiText.contains("I am Nexus")) {
-      return aiText.replaceAll("Nexus", "Spark");
-    }
-    return aiText;
-  }
-}
