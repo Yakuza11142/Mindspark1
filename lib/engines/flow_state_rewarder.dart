3f863beef3e685b593da4da0efbba430a1bbe0c6
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 16:32:48 +0100

    Delete flow_state_rewarder.dart

--- lib/engines/flow_state_rewarder.dart
+++
@@ -1,7 +0,0 @@
-class FlowStateRewarder {
-  static int calculateBonus(int minutesInFlow) {
-    if (minutesInFlow >= 30) return 100; // Deep work reward
-    if (minutesInFlow >= 15) return 25;
-    return 0;
-  }
-}
