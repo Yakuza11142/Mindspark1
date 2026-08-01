9eafd6461998aeb446bc9ebc2baf7d2563f8490f
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 17:46:10 +0100

    Delete avatar_animation_sync.dart

--- lib/engines/avatar_animation_sync.dart
+++
@@ -1,8 +0,0 @@
-class AvatarAnimationSync {
-  static String currentAnimation = "Idle";
-
-  static String syncWithAudio(
-      bool isAudioPlaying, String idleAnim, String talkAnim) {
-    return isAudioPlaying ? talkAnim : idleAnim;
-  }
-}
