b166919f3a54e275b451a7ff7d2d3aba70f84d7b
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 16:41:07 +0100

    Delete dynamic_discount_injector.dart

--- lib/engines/dynamic_discount_injector.dart
+++
@@ -1,5 +0,0 @@
-class DynamicDiscountInjector {
-  static String generateOffer(String currencyCode) {
-    return "SECRET OFFER: Stay Pro for the next 3 months at 50% off. Claim now or lose it forever.";
-  }
-}
