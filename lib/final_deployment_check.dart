e17f738d27dcf23e181f064c81222990a83ebf3d
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 00:20:07 +0100

    Delete final_deployment_check.dart

--- lib/final_deployment_check.dart
+++
@@ -1,6 +0,0 @@
-void deploymentCheck() {
-  print("580 Scripts Loaded.");
-  print("Codemagic: Configured.");
-  print("Shake Report: Active.");
-  print("MindSpark Elite is ready for the world.");
-}
