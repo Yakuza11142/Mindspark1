b77b14f3da4b483e9f6711f4b8868630c7b66db0
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 20:18:11 +0100

    Delete study_analytics_model.dart

--- lib/models/study_analytics_model.dart
+++
@@ -1,9 +0,0 @@
-class StudyAnalytics {
-  final int totalMinutes;
-  final int questionsAnswered;
-  final int correctAnswers;
-  final String weakestSubject;
-
-  StudyAnalytics(this.totalMinutes, this.questionsAnswered, this.correctAnswers,
-      this.weakestSubject);
-}
