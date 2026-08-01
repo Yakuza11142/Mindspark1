ad69dee8c4640523e3043714e3469ccddba6b7e2
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 12:02:44 +0100

    Delete sarcasm_detector.dart

--- lib/engines/sarcasm_detector.dart
+++
@@ -1,6 +0,0 @@
-class SarcasmDetector {
-  static String preprocessPrompt(String input) {
-    // Appends a hidden instruction to the LLM to handle teen sarcasm
-    return "$input \n[SYSTEM NOTE: The user is a student. Detect and ignore sarcasm, focus on the academic question.]";
-  }
-}
