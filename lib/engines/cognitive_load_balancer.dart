11e73946756f90fc317f736ccc41e6ca59a2a9de
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 16:58:09 +0100

    Delete cognitive_load_balancer.dart

--- lib/engines/cognitive_load_balancer.dart
+++
@@ -1,8 +0,0 @@
-class CognitiveLoadBalancer {
-  static String suggestLearningMode(int minutesStudied) {
-    if (minutesStudied > 120) {
-      return "VISUAL_ONLY"; // Turn off text, show 3D/Video to rest the reading brain
-    }
-    return "TEXT_AND_VISUAL";
-  }
-}
