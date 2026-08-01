5fc5a525c2f508210c17c6a027cc1d1ba3c02bb9
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 00:19:47 +0100

    Delete final_dependency_injection.dart

--- lib/final_dependency_injection.dart
+++
@@ -1,9 +0,0 @@
-import 'package:provider/provider.dart';
-import 'services/currency_manager.dart';
-import 'services/ad_manager.dart';
-
-// Helper to clean up main.dart
-// 🚀 CI AUTO-REMOVED: List<SingleChildCloneableWidget> get globalProviders => [
-      ChangeNotifierProvider(create: (_) => CurrencyManager()),
-      Provider(create: (_) => AdManager()),
-    ];
