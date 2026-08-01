ea735084d75d57c843d31e4912e67ae77180968b
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 18:56:35 +0100

    Delete apk_xender_beacon.dart

--- lib/engines/apk_xender_beacon.dart
+++
@@ -1,7 +0,0 @@
-// Requires 'apk_extractor' or native channel
-class ApkXenderBeacon {
-  static void generateExtractionQr() {
-    print("📡 Extracting com.mindspark.elite.apk from device storage...");
-    print("QR Code ready. Scan to download locally without data.");
-  }
-}
