8b475c70e679edddc08bce0dc95ace512179d5a0
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 11:20:51 +0100

    Update ssml_humanizer.dart

--- lib/engines/ssml_humanizer.dart
+++ lib/engines/ssml_humanizer.dart
@@ -1,12 +1,17 @@
 class SsmlHumanizer {
   static String injectHumanBreathing(String rawText, String ahName) {
-    // Adds microscopic pauses so the AI sounds like it is breathing in air
-    String humanized = rawText.replaceAll(".", ". <break time='400ms'/>");
+    // Escape the dot literal so it only matches periods
+    String humanized = rawText.replaceAll("\$", "\\\$"); // Escape inner symbols if needed
+    humanized = rawText.replaceAll(".", ". <break time='400ms'/>");
     humanized = humanized.replaceAll(",", ", <break time='200ms'/>");
 
-    if (HologramName == "Spark") {
-      // Spark sighs before explaining things
-      return "<phoneme alphabet='ipa' ph='h'>huh</phoneme> <break time='500ms'/> $humanized";
+    // Corrected to use RegExp.escape or literal split/join to avoid regex wildcard issues:
+    humanized = rawText.split('.').join('. <break time=\'400ms\'/>');
+    humanized = humanized.split(',').join(', <break time=\'200ms\'/>');
+
+    // Fixed variable name and ensured valid root SSML tags
+    if (ahName == "Spark") {
+      return "<speak><phoneme alphabet='ipa' ph='h'>huh</phoneme> <break time='500ms'/> $humanized</speak>";
     }
 
     return "<speak>$humanized</speak>";
