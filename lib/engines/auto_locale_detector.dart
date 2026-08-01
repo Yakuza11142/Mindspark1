eb4b0dbd19c082730386f7509e72e68aa4353b6c
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-01 17:46:19 +0100

    Delete auto_locale_detector.dart

--- lib/engines/auto_locale_detector.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui' as ui;
-
-class AutoLocaleDetector {
-  static String getDeviceLanguageCode() {
-    // Returns 'en', 'fr', 'zh', 'hi', etc.
-    return ui.PlatformDispatcher.instance.locale.languageCode;
-  }
-}
