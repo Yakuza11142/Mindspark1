95a87c5965e4fbd132609b8c03ffa9e86be05713
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 15:09:15 +0100

    Delete offline_data_purger.dart

--- lib/services/offline_data_purger.dart
+++
@@ -1,9 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class OfflineDataPurger {
-  static Future<void> clearOldCache() async {
-    final prefs = await SharedPreferences.getInstance();
-    // Logic to clear old keys
-    print("🧹 Old Cache Cleared. Storage Optimized.");
-  }
-}
