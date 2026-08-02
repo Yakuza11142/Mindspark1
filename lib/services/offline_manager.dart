2f9fcba3c63cdefa132ff342166d98863caa8ad9
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 15:08:12 +0100

    Delete offline_manager.dart

--- lib/services/offline_manager.dart
+++
@@ -1,8 +0,0 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-class OfflineManager {
-  static Future<bool> hasInternet() async {
-    var result = await Connectivity().checkConnectivity();
-    return result != ConnectivityResult.none;
-  }
-}
