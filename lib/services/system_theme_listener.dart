86e8585ce8db06da9f0a052d8bc59700fbd9e223
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 01:55:10 +0100

    Delete system_theme_listener.dart

--- lib/services/system_theme_listener.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SystemThemeListener with WidgetsBindingObserver {
-  @override
-  void didChangePlatformBrightness() {
-    print("OS Theme changed. Adapting MindSpark UI...");
-  }
-}
