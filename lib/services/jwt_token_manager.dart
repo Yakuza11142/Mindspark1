0d72fe2ea0c138bda55c7eed5284bdfc78f00b45
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 18:26:40 +0100

    Delete jwt_token_manager.dart

--- lib/services/jwt_token_manager.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class JwtTokenManager {
-  static Future<void> saveToken(String token) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('session_token', token);
-  }
-}
