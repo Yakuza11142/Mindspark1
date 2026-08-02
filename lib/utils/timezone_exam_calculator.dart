672afda4e18e4db6adaeac23e5d815b311edf105
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 20:51:15 +0100

    Delete timezone_exam_calculator.dart

--- lib/utils/timezone_exam_calculator.dart
+++
@@ -1,6 +0,0 @@
-class TimezoneExamCalculator {
-  static Duration getTimeUntilExam(DateTime examDateUtc) {
-    DateTime localNow = DateTime.now(); // Automatically uses device's timezone
-    return examDateUtc.difference(localNow);
-  }
-}
