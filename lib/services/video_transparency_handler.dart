bc582218f68a0c580bbcd844255624616080e084
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 01:18:05 +0100

    Delete video_transparency_handler.dart

--- lib/services/video_transparency_handler.dart
+++
@@ -1,6 +0,0 @@
-class VideoTransparencyHandler {
-  static void enableAlphaChannel() {
-    // Advanced WebRTC / VideoPlayer rendering to preserve alpha (transparency) channel
-    print("Alpha Channel Rendering Active for HeyGen Avatars.");
-  }
-}
