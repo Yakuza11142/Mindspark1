13f4f9ac42f94a88ee3a1d10fc2837c638609e96
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 15:11:26 +0100

    Delete offline_ad_cacher.dart

--- lib/services/offline_ad_cacher.dart
+++
@@ -1,9 +0,0 @@
-class OfflineAdCacher {
-  static void cacheAdsWhileOnWifi() {
-    print("Caching 5 AdMob videos to local storage for offline viewing.");
-  }
-
-  static void reportOfflineWatchesWhenOnline() {
-    print("Syncing offline ad views to Google to claim revenue.");
-  }
-}
