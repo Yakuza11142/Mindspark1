b12ed58558273fc1b2b06928499402ec25c52c84
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 20:13:01 +0100

    Delete platform_check.dart

--- lib/utils/platform_check.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-class PlatformCheck {
-  static bool get isAmazon =>
-      Platform.isAndroid &&
-      (File('/system/framework/com.google.android.maps.jar').existsSync() ==
-          false);
-}
