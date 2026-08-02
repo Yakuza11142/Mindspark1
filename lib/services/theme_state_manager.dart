2687dbd061b20ea33ba7426705b7e9c018d3d100
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 01:53:09 +0100

    Delete theme_state_manager.dart

--- lib/services/theme_state_manager.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ThemeStateManager extends ChangeNotifier {
-  final ThemeMode _mode = ThemeMode.dark; // Hardcoded to dark mode per CEO
-  ThemeMode get mode => _mode;
-}
