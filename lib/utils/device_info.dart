842c33f0914f78275c6ea50fe3ab530630549155
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 19:48:08 +0100

    Delete device_info.dart

--- lib/utils/device_info.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-class DeviceInfo {
-  static String getPlatform() {
-    if (Platform.isAndroid) return "Android Device";
-    if (Platform.isIOS) return "iOS Device";
-    return "Unknown Platform";
-  }
-}
