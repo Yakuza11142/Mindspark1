be7b8fbc5759a09c79256bee6fa35df88e6efb18
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 14:14:17 +0100

    Delete adaptive_difficulty.dart

--- lib/services/adaptive_difficulty.dart
+++
@@ -1,7 +0,0 @@
-class AdaptiveDifficulty {
-  static String getNextLevel(int currentStreak) {
-    if (currentStreak > 5) return "Hard";
-    if (currentStreak > 2) return "Medium";
-    return "Easy";
-  }
-}
