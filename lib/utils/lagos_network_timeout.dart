3d977408dab18ae8aff2065b6e63ef50f351b02a
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 19:51:00 +0100

    Delete lagos_network_timeout.dart

--- lib/utils/lagos_network_timeout.dart
+++
@@ -1,5 +0,0 @@
-class LagosNetworkTimeout {
-  // Instead of a standard 5-second timeout, we give Nigerian networks 15 seconds.
-  static const int connectTimeoutMs = 15000;
-  static const int receiveTimeoutMs = 20000;
-}
