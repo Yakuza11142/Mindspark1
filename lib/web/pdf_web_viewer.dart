74db9193ad9363e58eaeac0e8e0a39f56a97d5f1
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 19:16:04 +0100

    Delete pdf_web_viewer.dart

--- lib/web/pdf_web_viewer.dart
+++
@@ -1,7 +0,0 @@
-// Requires printing/pdf package web implementations
-class PdfWebViewer {
-  static void openInNewTab(String pdfDataUrl) {
-    print("Opening PDF Certificate in a new browser tab.");
-    // Implementation uses html.window.open
-  }
-}
