d6015ecebcd61b6ce40be1145a2cc0ec364fd20a
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 01:59:10 +0100

    Delete supabase_offline_sync.dart

--- lib/services/supabase_offline_sync.dart
+++
@@ -1,6 +0,0 @@
-class SupabaseOfflineSync {
-  static void syncNow() {
-    print("🌍 Network restored. Syncing local scores to Supabase Cloud...");
-    // Read local cache and batch insert to Supabase
-  }
-}
