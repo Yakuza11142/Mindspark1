5622e244802c7e69ccd9d3cc63ad793088bad24a
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 15:08:06 +0100

    Delete offline_mode_detector.dart

--- lib/services/offline_mode_detector.dart
+++
@@ -1,8 +0,0 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-class OfflineModeDetector {
-  static Future<bool> isOffline() async {
-    var res = await Connectivity().checkConnectivity();
-    return res == ConnectivityResult.none;
-  }
-}
