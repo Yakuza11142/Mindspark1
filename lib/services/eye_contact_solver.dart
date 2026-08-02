18e7ce9dec6d9d285f7ba0e126eedf270891b37c
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 18:51:03 +0100

    Delete eye_contact_solver.dart

--- lib/services/eye_contact_solver.dart
+++
@@ -1,6 +0,0 @@
-class EyeContactSolver {
-  static String enableLookAtCamera() {
-    print("👀 Eye IK Target locked to user's physical phone camera lens.");
-    return "camera-target='auto'";
-  }
-}
