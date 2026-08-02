c57fced489873b293b5aef5d88b585e55e9150a9
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 20:23:13 +0100

    Delete secure_payload_builder.dart

--- lib/utils/secure_payload_builder.dart
+++
@@ -1,7 +0,0 @@
-class SecurePayloadBuilder {
-  static Map<String, dynamic> build(Map<String, dynamic> data, String hwId) {
-    data['signed_hw_id'] = hwId;
-    data['timestamp'] = DateTime.now().toIso8601String();
-    return data;
-  }
-}
