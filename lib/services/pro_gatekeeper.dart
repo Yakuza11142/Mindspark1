35467a3d1626b27def3aa0b5ee13d1ce759d85f6
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 02:00:36 +0100

    Delete pro_gatekeeper.dart

--- lib/services/pro_gatekeeper.dart
+++
@@ -1,9 +0,0 @@
-class ProGatekeeper {
-  static bool verify(bool isPro) {
-    if (!isPro) {
-      print("🛑 BLOCK: Free user attempted to access Paid API.");
-      return false;
-    }
-    return true;
-  }
-}
