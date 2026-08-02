7231ce2e21ad5fb30a74b7b578702ee9b27f86ff
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 01:55:17 +0100

    Delete system_prompt_injector.dart

--- lib/services/system_prompt_injector.dart
+++
@@ -1,7 +0,0 @@
-import '../engines/live_config_listener.dart';
-
-class SystemPromptInjector {
-  static String buildPrompt(String basePrompt) {
-    return "${LiveConfigListener.liveSystemPrompt} \n\n Task: $basePrompt";
-  }
-}
