2ecd356ea18ec18a74bcb40341600bc30d85d9ad
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 19:47:11 +0100

    Delete date_utils_custom.dart

--- lib/utils/date_utils_custom.dart
+++
@@ -1,8 +0,0 @@
-class DateUtilsCustom {
-  static String timeAgo(DateTime d) {
-    Duration diff = DateTime.now().difference(d);
-    if (diff.inDays > 0) return "${diff.inDays} days ago";
-    if (diff.inHours > 0) return "${diff.inHours} hours ago";
-    return "Just now";
-  }
-}
