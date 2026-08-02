d51d34fec87d3551927a9bd6e236720e288f1219
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 15:07:13 +0100

    Delete offline_peer_sync.dart

--- lib/services/offline_peer_sync.dart
+++
@@ -1,6 +0,0 @@
-class OfflinePeerSync {
-  static void exchangeTokens(String peerData) {
-    // Encrypt and store peer XP locally until internet returns
-    print("Synced offline data with peer.");
-  }
-}
