5e3f2091367f41b81b82857e7e074938e8807795
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 15:06:54 +0100

    Delete offline_streak_reconciler.dart

--- lib/services/offline_streak_reconciler.dart
+++
@@ -1,6 +0,0 @@
-class OfflineStreakReconciler {
-  static int syncOfflineDays(int cloudStreak, int offlineDaysLogged) {
-    print("Syncing $offlineDaysLogged offline study days to Supabase...");
-    return cloudStreak + offlineDaysLogged;
-  }
-}
