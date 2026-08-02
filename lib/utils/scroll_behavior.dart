084589ebba0f0c2f71421e889309e1d87003a8ab
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 20:19:23 +0100

    Update scroll_behavior.dart

--- lib/utils/scroll_behavior.dart
+++ lib/utils/scroll_behavior.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 class NoGlowScrollBehavior extends ScrollBehavior {
   @override
-  Widget buildOverscrollIndicator(
-      BuildContext context, Widget child, ScrollableDetails details) {
+  Widget buildScrollbar(BuildContext context, Widget child, ScrollableDetails details) {
+    // 1. Returning the direct child without a Scrollbar wrapper cleans up layout lines
     return child;
   }
+
+  @override
+  GestureVelocityTrackerBuilder? buildVelocityTracker(BuildContext context) {
+    return super.buildVelocityTracker(context);
+  }
+
+  // 2. Modern way to strip the android glow/stretching effect across your application
+  @override
+  Set<PointerDeviceKind> get dragDevices => super.dragDevices;
 }
