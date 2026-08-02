53893717a760a62d6f542c483308533ce5258afc
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 19:17:48 +0100

    Delete app_lifecycle_detached.dart

--- lib/utils/app_lifecycle_detached.dart
+++
@@ -1,6 +0,0 @@
-class AppLifecycleDetached {
-  static void executeCleanup() {
-    // Release video players, close database connections
-    print("App killed by user. Releasing all hardware resources.");
-  }
-}
