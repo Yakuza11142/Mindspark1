6a72972a4cb4d12282792c217dcd147e895ce550
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 19:46:53 +0100

    Update date_helper.dart

--- lib/utils/date_helper.dart
+++ lib/utils/date_helper.dart
@@ -1,5 +1,9 @@
 import 'package:intl/intl.dart';
 
 class DateHelper {
-  static String getToday() => DateFormat('yyyy-MM-dd').format(DateTime.now());
+  // Adding an optional parameter allows for predictable unit testing and mock data validation
+  static String getToday([DateTime? customDate]) {
+    final targetDate = customDate ?? DateTime.now();
+    return DateFormat('yyyy-MM-dd', 'en_US').format(targetDate);
+  }
 }
