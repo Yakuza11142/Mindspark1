e8c952b2c051d9f9359b4a147b773b17f6db2f36
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 18:31:08 +0100

    Delete exam_time_distorter.dart

--- lib/services/exam_time_distorter.dart
+++
@@ -1,6 +0,0 @@
-class ExamTimeDistorter {
-  static int applyDistortion(int actualSecondsPassed) {
-    // 1 real second = 1.1 simulated seconds
-    return (actualSecondsPassed * 1.1).round();
-  }
-}
