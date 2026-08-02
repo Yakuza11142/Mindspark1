1fe913c90548fbb151d3bdab392eeee04ac14884
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-02 18:27:12 +0100

    Delete local_database.dart

--- lib/services/local_database.dart
+++
@@ -1,6 +0,0 @@
-// Placeholder for sqflite logic
-class LocalDatabase {
-  static Future<void> saveUserProgress(int xp, int level) async {
-    // SQL: INSERT INTO user (xp, level) VALUES (...)
-  }
-}
