4e687d220dd6303586a268c395165da03df8c59f
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-15 16:27:20 +0100

    Delete system_finalizer.dart

--- lib/system_finalizer.dart
+++
@@ -1,8 +0,0 @@
-class SystemFinalizer {
-  static const bool isReady = true;
-  static const int scriptCount = 300;
-
-  static void launch() {
-    print("MindSpark Elite: All systems go. 300 Scripts Loaded.");
-  }
-}
