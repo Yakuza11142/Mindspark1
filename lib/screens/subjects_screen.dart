41cb183728d61327af3251cdf48164b891bfedfe
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-20 14:54:21 +0100

    Create lib/screens/subjects_screen.dart

---
+++ lib/screens/subjects_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SubjectsScreen extends StatelessWidget {
+  const SubjectsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Subjects')),
+      body: const Center(
+        child: Text(
+          'Explore Subjects',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
