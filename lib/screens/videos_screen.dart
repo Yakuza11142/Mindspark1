49e6db95aea7bafb622ef8c8ac808b47a187c456
Yakuza11142 <yakubupeter785@gmail.com>
2026-08-20 14:54:48 +0100

    Create lib/screens/videos_screen.dart

---
+++ lib/screens/videos_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class VideosScreen extends StatelessWidget {
+  const VideosScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Videos')),
+      body: const Center(
+        child: Text(
+          'Educational Videos',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
